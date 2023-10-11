f70ceac05b08dc53207e6445d81cf62de0c20a30
Oleh Zankiv <zankiv.o@yoju.casino>
2023-10-11 08:10:39 +0300

    Module 1, Lection 1, finished

---
+++ module-01/lesson-01/js/01-vars.js
@@ -0,0 +1,50 @@
+// const x = 4;
+
+// name = "Ivan"
+ 
+// let age;
+
+// const name = "Oleh";
+// const surname = "Zankiv";
+// const position = "teacher";
+
+// const fullName = name + " " + surname + " is " + position;
+// const fullName = `${name} ${surname} is ${position}`;
+
+// const x = 11
+
+// console.log(x % 5);
+
+/**
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let и const
+ * - Перевизначення значення
+ * - Найменування змінних
+ * - Вибір між const і let
+ * - Числа, рядки, boolean, null, undefined
+ * - Оператор typeof
+ */
+
+
+const name = "Oleh";
+// let age = 23;
+
+// Year
+// age = 24;
+
+// const tasksListOfUser = 10;
+// const BASE_URL = 'http://www.com';
+// const tasks-list-of-user = 10; // error
+
+// console.log(name);
+// console.log(age);
+
+
+// const str = "Hello \"World\""; // string
+// const num = 2048; // number
+// const isAdmin = true; // boolean
+// const user = null; // null
+// let task; // undefined
+
+// console.log(typeof(task));
---
+++ module-01/lesson-01/js/04-strings.js
@@ -0,0 +1,46 @@
+/**
+ * Рядки
+ *
+ * - Конкатенація рядків
+ * - Шаблонні рядки
+ * - Довжина рядка
+ * - Індексація елементів
+ * - Незмінність рядків
+ */
+
+// const firstName = "Chelsy";
+// const lastName = "Emerald";
+
+// const fullName = firstName + " " + lastName
+// console.log(fullName);
+
+// console.log("1" + 5 * 5);
+
+// const quantity = 15;
+// const orderMsg = `You ordered ${quantity * 2} bots`;
+
+
+// console.log(orderMsg);
+
+
+// const message = "This string is 28 chars long";
+// // console.log(message);
+
+/** Задача 1
+ * Склади фразу за допомогою шаблонних рядків
+ * A has B bots in stock, де A, B - змінні вставлені в рядок.
+ */
+
+// const companyName = "Systems";
+// const repairBots = 100;
+// const defenceBots = 50;
+
+// const msg = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// console.log(msg); // "Cyberdyne Systems has 200 bots in stock"
+
+// console.log(companyName[0]);
+
+// const name = "Oleh";
+// const surname = "Zankiv"; // ( OZ )
+
+// name[0] + surname[0]
