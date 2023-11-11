7a23b9cbc659c1c130a3dd599188c705073afb02
Oleh Zankiv <zankiv.o@yoju.casino>
2023-11-11 16:00:53 +0200

    Module 6, Lection 11, init

---
+++ module-06/lesson-11/js/03-methods.js
@@ -0,0 +1,30 @@
+/**
+ * Контекст виконання функції
+ *
+ * - Метод call
+ * - Метод apply
+ * - Метод bind
+ */
+
+// const showThis = function() {
+//   console.log(this);
+// }
+
+/**
+ * -------------------------------
+ */
+// const counter = {
+//   value: 0,
+//   increment(value) {
+//     console.log("increment -> this", this);
+//     this.value += value;
+//   },
+//   decrement(value) {
+//     console.log("decrement -> this", this);
+//     this.value -= value;
+//   },
+// };
+
+// const updateCounter = function (value, operation) {
+//   operation(value);
+// };
