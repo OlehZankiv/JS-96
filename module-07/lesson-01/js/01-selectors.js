b90cabadb455d1e5449052bff9330efdfd02ba5b
Oleh Zankiv <zankiv.o@yoju.casino>
2023-11-26 16:21:21 +0200

    Module 7, Lection 13, init

---
+++ module-07/lesson-01/js/01-selectors.js
@@ -0,0 +1,18 @@
+/*
+* - Що таке DOM та DOM-дерево?
+* - Як браузер підтягує файли?
+*/
+
+/*
+ * Пошук HTML елементу за допомогою querySelector та querySelectorAll
+ * - За назвою тегу
+ * - За назвою класу
+ * - За ID
+ */
+
+
+/*
+ * Властивості «навігації» по DOM-вузлах
+ * - Різниця між HTML-вузлом та HTML-елементом
+ * - children, childNodex, firstChild, firstElementChild ...
+ */
---
+++ module-07/lesson-01/js/04-create.js
@@ -0,0 +1,23 @@
+/*
+ * Створення та додавання елементів
+ */
+
+// const title = document.createElement("h1");
+
+/*
+ * Створюємо заголовок
+ */
+// const titleEl = document.createElement('h1');
+
+/*
+ * Створюємо зображення
+ * https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg
+ * valais-alpine-mountains-glacier
+ */
+// const imageEl = document.createElement('img');
+
+// const heroEl = document.querySelector('.hero');
+
+/*
+ * Створюємо та додаємо новий пункт меню
+ */
